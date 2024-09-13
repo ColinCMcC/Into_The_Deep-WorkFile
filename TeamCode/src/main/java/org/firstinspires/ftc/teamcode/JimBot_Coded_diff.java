0a91ccb758e13dc9562c967b1f0031bb72ec4e49
ColinCMcC <118211798+ColinCMcC@users.noreply.github.com>
2024-09-13 00:13:25 -0500

    9-12-24
    Did a lot of cleanup stuff.
    Added an enum for turning direction

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JimBot_Coded_diff.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JimBot_Coded_diff.java
@@ -1,15 +1,21 @@
 package org.firstinspires.ftc.teamcode;
 
+// Static imports for ENUMs that are used a lot
+
 import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.BRAKE;
 import static com.qualcomm.robotcore.hardware.DcMotorSimple.Direction.FORWARD;
 import static com.qualcomm.robotcore.hardware.DcMotorSimple.Direction.REVERSE;
+import static org.firstinspires.ftc.teamcode.JimBot_Coded_diff.turnDir.LEFT;
+import static org.firstinspires.ftc.teamcode.JimBot_Coded_diff.turnDir.RIGHT;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
+
 // This file is testing out driving the robot using calculated wheel speeds for turning while driving
+// For turning, 0° is straight forward, 90° is to the right, and -90° is to the left
 
 @TeleOp(name = "Curved Steering OpMode")
 public class JimBot_Coded_diff extends LinearOpMode {
@@ -17,16 +23,20 @@ public class JimBot_Coded_diff extends LinearOpMode {
     // Constants for robot geometry
     private static final double WHEELBASE = 13; // Wheelbase (distance between front and rear axles) in IN.
     private static final double TRACK_WIDTH = 14; // Track width (distance between left and right wheels) in IN.
-    // Define the motors
-    DcMotor FLMotor;
-    DcMotor FRMotor;
-    DcMotor BLMotor;
-    DcMotor BRMotor;
+    private static final double WHEEL_DIAMETER = 3; // Wheel diameter in IN.
 
+
+    // Create motor instances
+    DcMotor FLMotor, FRMotor, BLMotor, BRMotor;
+
+    // Create servo instances
     Servo FLServo, BLServo, BRServo, FRServo;
+
 
     @Override
     public void runOpMode() {
+
+
         // Initialize hardware stuff
         initRobot();
 
@@ -42,16 +52,22 @@ public class JimBot_Coded_diff extends LinearOpMode {
             double turningRadius = calculateTurningRadius(turn); // Radius in IN.
             double angularVelocity = drive / turningRadius; // Calculate angular velocity (rad/s)
 
-            // Calculates the speeds for each motor
+            // Calculates the speeds and angles for each motor
             double[] wheelSpeed = calculateWheelSpeed(angularVelocity, turningRadius);
             double[] wheelAngle = calculateWheelAngles(turningRadius);
 
-            moveIt(wheelSpeed, wheelAngle);
+
+            // If the gamepad is turning right, turn the robot right, otherwise turn left
+            turnDir direction = LEFT;
+            if (turn < 0) direction = RIGHT;
+
+            moveIt(wheelSpeed, wheelAngle, direction);
 
         }
     }
 
 
+    // Configures all the stuff for the robot
     public void initRobot() {
 
         // Maps where the motors are physically connected
@@ -90,60 +106,76 @@ public class JimBot_Coded_diff extends LinearOpMode {
     }
 
 
-    public void moveIt(double wheelSpeed[], double wheelAngle[]) {
+    // Moves the robot
+    // wheelSpeed[] = Four parameters - FLSpeed, FRSpeed, BLSpeed, BRSpeed
+    // wheelAngle[] = Two parameters - Inner angle, Outer angle
+    // turnDirection = Direction to turn the robot - LEFT or RIGHT
+    public void moveIt(double[] wheelSpeed, double[] wheelAngle, turnDir dir) {
 
-        if (wheelAngle[0] > 0) // Turning right
+        if (dir == RIGHT) // Turning right
         {
             // Set power to motors based on calculated speeds
             FLMotor.setPower(wheelSpeed[0]); // Outside Front
             FRMotor.setPower(wheelSpeed[1]); // Inside Front
             BLMotor.setPower(wheelSpeed[2]); // Outside Rear
             BRMotor.setPower(wheelSpeed[3]); // Inside Rear
-        } else if (wheelAngle[0] < 0) // Turning left
+
+            // Sets the custom positions of the wheels
+            FLServo.setPosition(wheelAngle[1]); // Outer
+            FRServo.setPosition(wheelAngle[0]); // Inner
+            BLServo.setPosition(-wheelAngle[1]); // Outer. Negative because the rear wheels need to turn the opposite direction
+            BRServo.setPosition(-wheelAngle[0]); // Inner
+
+        } else // Turning Left
         {
             // Set power to motors based on calculated speeds
             FLMotor.setPower(wheelSpeed[0]); // Inside Front
             FRMotor.setPower(wheelSpeed[1]); // Outside Front
             BLMotor.setPower(wheelSpeed[2]); // Inside Rear
             BRMotor.setPower(wheelSpeed[3]); // Outside Rear
-        }
 
-        // Sets the custom positions of the wheels
-        FLServo.setPosition(wheelAngle[0]); // Left
-        FRServo.setPosition(wheelAngle[1]); // Right
-        BLServo.setPosition(wheelAngle[0]); // Left
-        BRServo.setPosition(wheelAngle[1]); // Right
+            // Sets the custom positions of the wheels
+            FLServo.setPosition(wheelAngle[0]); // Inner
+            FRServo.setPosition(wheelAngle[1]); // Outer
+            BLServo.setPosition(-wheelAngle[0]); // Inner. Negative because the rear wheels need to turn the opposite direction
+            BRServo.setPosition(-wheelAngle[1]); // Outer.
+        }
     }
 
 
-    // Function to calculate wheel speeds for a given angular velocity and turning radius
-    private double[] calculateWheelSpeed(double angularVelocity, double turningRadius) {
+    // Function to calculate wheel speeds for a given desired speed and turning radius
+    // desiredSpeed = Desired speed of the robot
+    private double[] calculateWheelSpeed(double desiredSpeed, double turningRadius) {
+
         // Inside and outside turning radii for front/rear wheels
         double R_inner = turningRadius - (TRACK_WIDTH / 2);
         double R_outer = turningRadius + (TRACK_WIDTH / 2);
 
-        // Calculate wheel speeds
-        double V_IF = angularVelocity * R_inner; // Inside Front
-        double V_OF = angularVelocity * R_outer; // Outside Front
-        double V_IB = angularVelocity * R_inner; // Inside Rear
-        double V_OB = angularVelocity * R_outer; // Outside Rear
+        // double I_Modifier = ;
 
-        // If any wheel speeds are above 1.0, scales all the others down so that it is at max speed
-        double maxSpeed = Math.max(Math.max(Math.abs(V_IF), Math.abs(V_OF)), Math.max(Math.abs(V_IB), Math.abs(V_OB)));
+        // Calculate wheel speeds
+        double S_IF = desiredSpeed * R_inner; // Inside Front
+        double S_OF = desiredSpeed * R_outer; // Outside Front
+        double S_IB = desiredSpeed * R_inner; // Inside Back
+        double S_OB = desiredSpeed * R_outer; // Outside Back
+
+        // If any wheel speeds are above 1.0, scales all the others down proportionally
+        double maxSpeed = Math.max(Math.max(Math.abs(S_IF), Math.abs(S_OF)), Math.max(Math.abs(S_IB), Math.abs(S_OB)));
         if (maxSpeed > 1.0) {
-            V_IF /= maxSpeed;
-            V_OF /= maxSpeed;
-            V_IB /= maxSpeed;
-            V_OB /= maxSpeed;
+            S_IF /= maxSpeed;
+            S_OF /= maxSpeed;
+            S_IB /= maxSpeed;
+            S_OB /= maxSpeed;
         }
 
-        return new double[]{V_IF, V_OF, V_IB, V_OB};
+        return new double[]{S_IF, S_OF, S_IB, S_OB};
     }
 
 
     // Calculates the inner and outer wheel angles for a given radius
     public double[] calculateWheelAngles(double turningRadius) {
 
+        // Takes the turning radius, and calculates the angle of a line tangent to it at the appropriate distance
         double inner = Math.atan(WHEELBASE / (turningRadius - (TRACK_WIDTH / 2)));
         double outer = Math.atan(WHEELBASE / (turningRadius + (TRACK_WIDTH / 2)));
 
@@ -164,4 +196,13 @@ public class JimBot_Coded_diff extends LinearOpMode {
         // Smaller turn input means tighter radius
         return maxTurnRadius / Math.abs(turn);
     }
+
+
+    // Used to store possible turning directions
+    enum turnDir {
+        LEFT, RIGHT
+    }
+
+
 }
+
